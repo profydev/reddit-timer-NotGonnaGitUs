530b7494160d14621603c53b7ed40b9b868ff564
NotGonnaGitUs <NotGonnaGitUs@users.noreply.github.com>
2021-10-03 11:10:28 +0530

    Create app skeleton (#1)
    * Adds normalize.css
    
    * Adds styled-components, fonts and global styles
    
    * Adds react router, home and search pages placeholders
    
    * Makes Google fonts hosted locally, adds theme setup, uses styled-normalize package instead of normalize and makes router default to root without redirect
    
    * Change app title, meta description and adds favicons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
+import GlobalStyle from './css/globalStyles';
+import theme from './theme';
+import Home from './pages/home';
+import Search from './pages/search';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Normalize />
+        <GlobalStyle />
+        <Switch>
+          <Route path="/search" component={Search} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
+
   );
 }
 
